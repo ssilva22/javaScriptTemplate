1fd1b134541b827a879ab8afdfd777df4b0215cb
Sebastian <sebastian@SEBASTIAN-01.local>
2021-06-29 19:19:38 -0400

    removed mongoDB and reinstalling

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const bodyParser= require('body-parser');
-const mongoose = require('mongoose');
+
 
 const app= express();
 
@@ -12,37 +12,6 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
-
-mongoose.connect("mongodb+srv://ssilva22:<password>@cluster0.30ueh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/todolistDB", {useNewUrlParser: true});
-
-const itemsSchema = {
-    name: String
-};
-
-const Item = mongoose.model("Item", itemsSchema);
-
-const item1 = new Item({
-    name: "Welcome to your todolist!"
-});
-
-const item2 = new Item({
-    name: `Hit the + button to add a new item`
-});
-
-const item3 = new Item({
-    name:  `<--Hit this to delete an item."`
-});
-
-const defaultItems=[item1,item2,item3];
-
-Item.insertMany(defaultItems , function(err){
-
-    if(err){
-        console.log(err);
-    }else{
-       console.log("Successfully saved default items to database") ;
-    }
-});
 
 app.get("/", function(req, res){
    const today= new Date();
