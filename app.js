d9cca1747a885da5812a8fd9d78978e9e3871123
Sebastian <sebastian@SEBASTIAN-01.local>
2021-06-28 20:26:37 -0400

    finished for the moment might add more features later

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser= require('body-parser');
 const app= express();
 
 let items= ['Buy Food','Eat Food','Cook Food'];
+let workItems=[];
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -25,16 +26,35 @@ app.get("/", function(req, res){
 
     
 
-   res.render('list', {kindOfDay: day, newListItems: items});
+   res.render('list', {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
-   item= req.body.newItem;
+    let item= req.body.newItem;
 
-   items.push(item);
-   res.redirect("/");
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
+
+app.get("/work", function(req,res){
+    res.render("list",{listTitle: "Work List" , newListItems: workItems});
+});
+
+app.post("/work" , function(req,res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work")
 })
 
+app.get("/about", function(req, res){
+    res.render("about");
+});
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
