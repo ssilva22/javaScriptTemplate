4e0eea32e349117dc85a406b3bf7cf02aa5fcd72
Sebastian <sebastian@SEBASTIAN-01.local>
2021-06-30 18:26:04 -0400

    delete command not working since method has been deprecated

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express= require('express');
+const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
 
 
 const app= express();
 
-let items= ['Buy Food','Eat Food','Cook Food'];
-let workItems=[];
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -13,38 +13,91 @@ app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true},{useUnifiedTopology:true});
+
+const itemsSchema= {
+    name: String,
+};
+
+const Item= mongoose.model("Item",itemsSchema);
+
+const item1= new Item({
+    name: "Welcome to your to-do list"
+});
+
+const item2= new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3= new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems= [item1,item2,item3];
+/*
+Item.insertMany(defaultItems, function(err){
+    if(err){
+        console.log(err)
+    }else{
+        console.log("Items have been successfully added")
+    }
+});
+*/
+
+
 app.get("/", function(req, res){
    const today= new Date();
    const currentDay= today.getDay();
 
    const options= {
-       weekday: "long",
-       day: "numeric",
-       month: "long",
-   };
-   
-   let day= today.toLocaleDateString("en-US",options);
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+};
 
-    
+let day= today.toLocaleDateString("en-US",options);
 
-   res.render('list', {listTitle: day, newListItems: items});
+   Item.find({}, function(err,foundItems){
+       if(foundItems.length === 0){
+        Item.insertMany(defaultItems, function(err){
+            if(err){
+                console.log(err)
+            }else{
+                console.log("Items have been successfully added")
+            }
+        })
+        res.redirect("/");
+       }else{
+           res.render('list', {listTitle: day, newListItems: foundItems});
+           }
+   });
+
 });
 
 app.post("/", function(req, res){
-    let item= req.body.newItem;
+    const itemName= req.body.newItem;
 
-    if(req.body.list === "Work"){
-        workItems.push(item);
-        res.redirect("/work");
-    }else{
-        items.push(item);
+    const item = new Item({
+        name: itemName
+    })
+    item.save()
+    res.redirect("/");
+});
+
+app.post("/delete", function(req,res){
+const checkedItemId= req.body.checkbox;
+console.log(checkedItemId)
+
+Item.findByIdAndRemove(checkedItemId, function(err){
+    if(!err){
+        console.log("Items have been successfully deleted");
         res.redirect("/");
     }
-
+})
 });
 
 app.get("/work", function(req,res){
-    res.render("list",{listTitle: "Work List" , newListItems: workItems});
+    res.render("list",{listTitle: "Work List" , newListItems: foundItems});
 });
 
 app.post("/work" , function(req,res){
